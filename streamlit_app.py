2a2671c6164c66afcb79564ce614d05cb26362fe
ZOEHE99 <hzj0621lucky@gmail.com>
2024-09-25 23:30:42 +0100

    Change API call location

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ from snowflake.snowpark.functions import col
 import requests
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -46,3 +44,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+ name_on_order + '!', icon="✅")
+
+#new section to display fruit data
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
